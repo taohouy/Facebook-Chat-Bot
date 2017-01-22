4dcee168b81ca9cce68ee7d3cafd3749b99cd112
taohouy <taohouy@gmail.com>
2017-01-22 16:39:03 +0700

    Update test.php

--- test.php
+++ test.php
@@ -33,8 +33,8 @@ if(preg_match('[time|current time|now]', strtolower($message))) {
     $message_to_reply = 'Huh! what do you mean?';
 }
 
-$sender = '100001907613482';
-$message_to_reply = 'Test';
+$sender = '1290539144347250';
+$message_to_reply = 'Tesกกกกกกt';
 
 //API Url
 $url = 'https://graph.facebook.com/v2.8/me/messages?access_token='.$access_token;
