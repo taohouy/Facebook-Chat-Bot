c373fdbaf4a30d830751c5d31bba2d6cf46cc7df
taohouy <taohouy@gmail.com>
2017-01-23 10:08:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $message_to_reply = '';
  * Some Basic rules to validate incoming messages
  */
 $gmessage = explode(":",$message);
-if('สมัครบริการแจ้งเตือน'==$gmessage[0])) {
+if("สมัครบริการแจ้งเตือน"==$gmessage[0])) {
     $stuid = $gmessage[1];
     // Make request to Time API
     //ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
